ced8167d7b0026770417a57f276a85af4b7ccca7
Lucas Cantero <ldanielcantero@gmail.com>
2023-03-14 19:41:49 +0100

    function Card modifié

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 import '../../styles/card.css'
 
-export default function Card({ cover, title }) {
+export default function Card({ data }) {
+  const { id, title, cover } = data
   return (
-    <article className="card-apart">
-      <img src={cover} alt="location" />
-      <div className="card-apart-layer">
-        <p className="card-apart-title">{title}</p>
-      </div>
+    <article>
+      <Link to={`/apartment/${id}`} className="link-card-apart">
+        <img src={cover} alt={title} className="card-img" />
+        <div className="card-div"></div>
+        <h2>{title}</h2>
+      </Link>
     </article>
   )
 }
