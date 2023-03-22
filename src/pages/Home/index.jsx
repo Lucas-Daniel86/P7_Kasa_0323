b6e93511704df7e2f3b162c4dc24fdaa7beff1a6
Lucas Cantero <ldanielcantero@gmail.com>
2023-03-22 21:46:54 +0100

    fichiers renommés et modifiés

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,15 @@
 import Banner from '../../components/Banner'
 import banner from '../../assets/images/banner-home.png'
-import Gallery from '../../components/Gallery'
+import Card from '../../components/Card'
 import accomodations from '../../data.json'
 
 export default function Home() {
   return (
     <>
       <Banner image={banner} title="Chez vous, partout et ailleurs" />
-      <section className="gallery-container">
+      <section className="card-container">
         {accomodations.map((apart) => (
-          <Gallery key={apart.id} data={apart} />
+          <Card key={apart.id} data={apart} />
         ))}
       </section>
     </>
