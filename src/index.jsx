55ba48ee62990215397b27c93dd1d6c381b538ee
Lucas Cantero <ldanielcantero@gmail.com>
2023-03-23 10:33:16 +0100

    fichier index modifié

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Apartment from './pages/Apartment'
-import Error from './pages/Error'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/apartment/:id" element={<Apartment />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
-    <Footer />
+    <App />
   </BrowserRouter>
 )
